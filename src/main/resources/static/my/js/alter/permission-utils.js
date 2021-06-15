f6fe57955b9873cb81d49dd8a49ddbe3aa5f84bd
Administrator <1589512343@qq.com>
2021-06-15 10:24:30 +0800

    初始化项目

---
+++ src/main/resources/static/my/js/alter/permission-utils.js
@@ -0,0 +1,5 @@
+function PermissionUtils() {
+
+}
+
+PermissionUtils.prototype = {}
---
+++ src/test/java/gdit/dormitory/DormitoryApplicationTests.java
@@ -0,0 +1,13 @@
+package gdit.dormitory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormitoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
